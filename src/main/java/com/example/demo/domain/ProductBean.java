9aeca5c704f9583b9f00921c3499a9f6dc652982
Ray <ray@test.com>
2022-12-29 12:32:37 +0800

    add productBean

---
+++ src/main/java/com/example/demo/domain/ProductBean.java
@@ -0,0 +1,76 @@
+package com.example.demo.domain;
+
+public class ProductBean {
+	private int id;
+	private String kind;
+	private String brand;
+	private String name;
+	private String color;
+	private String description;
+	private double cost;
+	private int stock;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getKind() {
+		return kind;
+	}
+
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getCost() {
+		return cost;
+	}
+
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+}
