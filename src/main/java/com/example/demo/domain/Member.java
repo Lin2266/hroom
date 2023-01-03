79d6bedfdd94d812d6c5f8a5ffff2854e5ef3c27
SHUWEIHSU <a0972324316@gmail.com>
2023-01-03 09:56:21 +0800

    member bean

--- src/main/java/com/example/demo/domain/Member.java
+++ src/main/java/com/example/demo/domain/Member.java
@@ -1,9 +1,14 @@
 package com.example.demo.domain;
 
 public class Member {
-	 private int id;
-	  private String name;
-	  private String email;
+	private int id;
+	private String name;
+	private String phone;
+	private String email;
+	private String city;
+	private String country;
+	private String zipcode;
+	private String address;
 	public int getId() {
 		return id;
 	}
@@ -16,11 +21,44 @@ public class Member {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	  
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+
+
 }
